6c69b7abd31e44077329b27c060bd65a7280c968
林博斯 <linbs91@gmail.com>
2018-09-15 13:44:31 -0700

    finished library and quotes page, allowing categories and corresponding quotes to be displayed

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -3,6 +3,7 @@ import { NavController, NavParams } from 'ionic-angular';
 import { QuotePage } from '../quote/quote';
 import { Quote } from '../../data/quote.interface';
 import quotes from '../../data/quotes';
+import { QuotesPage } from '../quotes/quotes';
 
 @Component({
   selector: 'page-library',
@@ -11,6 +12,8 @@ import quotes from '../../data/quotes';
 
 export class LibraryPage implements OnInit {
   quoteCollection: {category: string, quotes: Quote[], icon: string}[];
+  quotesPage = QuotesPage;
+  
   ngOnInit() { 
     this.quoteCollection = quotes;
   }
--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,25 +1,46 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { NavParams, AlertController } from 'ionic-angular';
+import { Quote } from '../../data/quote.interface';
 
-/**
- * Generated class for the QuotesPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-quotes',
   templateUrl: 'quotes.html',
 })
-export class QuotesPage {
+export class QuotesPage implements OnInit {
+  quoteGroup: {category: string, quotes: Quote[], icon: string};
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    private navParams: NavParams, 
+    public alertCtrl: AlertController) { }
+
+  ngOnInit() {
+    this.quoteGroup = this.navParams.data;
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad QuotesPage');
+  onAddToFavorite(selectedQuote: Quote) {
+    const alert = this.alertCtrl.create({
+      title: 'Add Quote',
+      message: 'Are you sure you want to add the quote?',
+      buttons: [
+        {
+        text: 'Yes',
+        handler: () => {
+          console.log("OK");
+        }
+      },
+      {
+        text: 'Cancel',
+        role: 'cancel',
+        handler: () => {
+          console.log("Cancelled")
+        }
+      }
+    ]
+    });
+    alert.present();
   }
-
+  // Add elvis operator (?) in template to use this approach
+  // ionViewDidLoad() {
+  //   this.quoteGroup = this.navParams.data;
+  // }
 }
